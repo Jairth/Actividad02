2b65d62d7fd72d2bbcf8e6b50f9cacc3d9171388
Jair Rodriguez <rodriguez6191@hotmail.com>
2025-01-28 22:10:02 -0500

    First commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
-import { Routes } from '@angular/router';
+import type { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'session',
+    loadChildren: () => import("./register/register.routes"),
+  },
+  {
+    path: "**",
+    redirectTo: "/session",
+  }
+];
